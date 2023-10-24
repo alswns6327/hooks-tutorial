5e0626281e0ce6096c1eccb5116314f210703bb5
alswns6327 <alswns6327@naver.com>
2023-10-25 00:23:18 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 import Counter from './Counter';
 import Info from './Info';
 import { useState } from 'react';
+import Average from './Average';
 
 function App() {
   const [visible, setVisible] = useState(false);
   return (
     <div>
-      <button onClick={()=>{setVisible(!visible)}}>
+      {/* <button onClick={()=>{setVisible(!visible)}}>
         {visible? '숨기기' : '보이기'}
       </button>
       <hr/>
-      {visible && <Info></Info>}
+      {visible && <Info></Info>} */}
       {/* <Counter></Counter> */}
+      <Average></Average>
     </div>
   );
 }
