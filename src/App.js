0a76998ebd751303f567eaaaa85a7a66fbbd5fcb
alswns6327 <alswns6327@naver.com>
2023-10-24 23:21:05 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
   const [visible, setVisible] = useState(false);
   return (
     <div>
-      <button onClick={()=>{setVisible(!visible)}}>
+      {/* <button onClick={()=>{setVisible(!visible)}}>
         {visible? '숨기기' : '보이기'}
       </button>
-      {/* <Counter></Counter> */}
       <hr/>
-      {visible && <Info></Info>}
+      {visible && <Info></Info>} */}
+      <Counter></Counter>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 
 const Counter = () => {
-    const [value, setValue] = useState(0);
+
+    function reducer(stated, action) {
+        switch(action.type){
+            case 'INCREMENT':
+                return stated + 1;
+            case 'DECREMENT':
+                return --stated;
+            default:
+                return stated;
+        }
+    }
+
+    const [statea, dispatch] = useReducer(reducer, 0);
 
     return (
         <div>
             <p>
-                현재 카운터 값 <b>{value}</b>
+                현재 카운터 값 <b>{statea}</b>
             </p>
-            <button onClick={() => setValue(value + 1)}>+1</button>
-            <button onClick={() => setValue(value - 1)}>-1</button>
+            <button onClick={() => dispatch({type : "INCREMENT"})}>+1</button>
+            <button onClick={() => dispatch({type : "DECREMENT"})}>-1</button>
         </div>
     );
 };
